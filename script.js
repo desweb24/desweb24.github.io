ec669824fe23a5bc9e99c01109e591ed5eedf877
desweb24 <24desweb@gmail.com>
2024-11-12 04:14:20 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 async function carregarImagens(urls, id) {
-    const headers = {
-        'Authorization': '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'  // Substitua com seu token do GitHub
-    };
+
+    const token  ='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';
 
     for (const url of urls) {
         const resposta = await fetch(url, {
@@ -9,11 +8,14 @@ async function carregarImagens(urls, id) {
                 Authorization: `token ${token}`
             }
         });
+
         if (resposta.ok) {
             const dados = await resposta.json();
             const imagemURL = dados.download_url;
+
             const imagemElemento = document.createElement('img');
             imagemElemento.src = imagemURL;
+
             document.getElementById(id).appendChild(imagemElemento);
         } else {
             console.error(`Erro ao carregar a imagem de ${url}: ${resposta.statusText}`);
@@ -22,19 +24,19 @@ async function carregarImagens(urls, id) {
 }
 
 
-const repo = 'desweb24/portfolio';
-document.addEventListener('DOMContentLoaded', function() {
+const repo = 'desweb24/desweb24.github.io';
+
+//document.addEventListener('DOMContentLoaded', function() {
 //Imagens Página Index
 const img1 =[
-    `https://api.github.com/repos/${repo}/contents/homepage.jpeg`
+    `https://api.github.com/repos/${repo}/contents/folder/homepage.jpeg`
 ];
-
 
 const imgRS = [
-    `https://api.github.com/repos/${repo}/contents/rockstar1.png`
+    `https://api.github.com/repos/${repo}/contents/folder/rockstar1.png`
 ];
 const imgPIT = [
-    `https://api.github.com/repos/${repo}/contents/picit1.png`
+    `https://api.github.com/repos/${repo}/contents/folder/picit1.png`
 ];
 
 carregarImagens(img1,'img1');
@@ -43,29 +45,29 @@ carregarImagens(imgPIT,'imgPIT');
 
 //Imagens Página Sobre
 const imgSobre = [
-    `https://api.github.com/repos/${repo}/contents/sobre.JPG`
+    `https://api.github.com/repos/${repo}/contents/folder/sobre.JPG`
 ];
 
 carregarImagens(imgSobre,'imgSobre');
 
 //imagem fotografia
 const ft = [
-    `https://api.github.com/repos/${repo}/contents/fotografia.png`
+    `https://api.github.com/repos/${repo}/contents/folder/fotografia.png`
 ];
 const foto_jps = [
-    `https://api.github.com/repos/${repo}/contents/fotografia1_1.jpeg`,
-    `https://api.github.com/repos/${repo}/contents/fotografia1_2.jpeg`,
-    `https://api.github.com/repos/${repo}/contents/fotografia1_3.jpeg`,
-    `https://api.github.com/repos/${repo}/contents/fotografia1_4.jpeg`,
-    `https://api.github.com/repos/${repo}/contents/fotografia1_5.jpeg`,
-    `https://api.github.com/repos/${repo}/contents/fotografia1_6.jpeg`
+    `https://api.github.com/repos/${repo}/contents/folder/fotografia1_1.jpeg`,
+    `https://api.github.com/repos/${repo}/contents/folder/fotografia1_2.jpeg`,
+    `https://api.github.com/repos/${repo}/contents/folder/fotografia1_3.jpeg`,
+    `https://api.github.com/repos/${repo}/contents/folder/fotografia1_4.jpeg`,
+    `https://api.github.com/repos/${repo}/contents/folder/fotografia1_5.jpeg`,
+    `https://api.github.com/repos/${repo}/contents/folder/fotografia1_6.jpeg`
 ];
 const foto_fw = [
-    `https://api.github.com/repos/${repo}/contents/fotografia2_1.jpeg`,
-    `https://api.github.com/repos/${repo}/contents/fotografia2_2.jpeg`,
-    `https://api.github.com/repos/${repo}/contents/fotografia2_3.jpeg`,
-    `https://api.github.com/repos/${repo}/contents/fotografia2_4.jpeg`,
-    `https://api.github.com/repos/${repo}/contents/fotografia2_5.jpeg`
+    `https://api.github.com/repos/${repo}/contents/folder/fotografia2_1.jpeg`,
+    `https://api.github.com/repos/${repo}/contents/folder/fotografia2_2.jpeg`,
+    `https://api.github.com/repos/${repo}/contents/folder/fotografia2_3.jpeg`,
+    `https://api.github.com/repos/${repo}/contents/folder/fotografia2_4.jpeg`,
+    `https://api.github.com/repos/${repo}/contents/folder/fotografia2_5.jpeg`
 ];
 
 
@@ -75,29 +77,29 @@ carregarImagens(foto_fw,'foto_fw');
 
 // Imagens Rocktar
 const rs1 = [
-    `https://api.github.com/repos/${repo}/contents/rockstar1.png`,
-    `https://api.github.com/repos/${repo}/contents/rockstar3.png`,
+    `https://api.github.com/repos/${repo}/contents/folder/rockstar1.png`,
+    `https://api.github.com/repos/${repo}/contents/folder/rockstar3.png`,
 ];
 const mup1 = [
-    `https://api.github.com/repos/${repo}/contents/mockup_user1.png`,
-    `https://api.github.com/repos/${repo}/contents/mockup_user2.png`,
-    `https://api.github.com/repos/${repo}/contents/mockup_user3.png`,
+    `https://api.github.com/repos/${repo}/contents/folder/mockup_user1.png`,
+    `https://api.github.com/repos/${repo}/contents/folder/mockup_user2.png`,
+    `https://api.github.com/repos/${repo}/contents/folder/mockup_user3.png`,
 ];
 const mup2 = [
-    `https://api.github.com/repos/${repo}/contents/mockup_musico1.png`,
-    `https://api.github.com/repos/${repo}/contents/mockup_musico2.png`,
-    `https://api.github.com/repos/${repo}/contents/mockup_musico3.png`,
+    `https://api.github.com/repos/${repo}/contents/folder/mockup_musico1.png`,
+    `https://api.github.com/repos/${repo}/contents/folder/mockup_musico2.png`,
+    `https://api.github.com/repos/${repo}/contents/folder/mockup_musico3.png`,
 ];
 
 const rock_pf1 = [
-    `https://api.github.com/repos/${repo}/contents/pf_user1.png`,
-    `https://api.github.com/repos/${repo}/contents/pf_user2.png`,
-    `https://api.github.com/repos/${repo}/contents/pf_user3.png`,
+    `https://api.github.com/repos/${repo}/contents/folder/pf_user1.png`,
+    `https://api.github.com/repos/${repo}/contents/folder/pf_user2.png`,
+    `https://api.github.com/repos/${repo}/contents/folder/pf_user3.png`,
 ];
 const rock_pf2 = [
-    `https://api.github.com/repos/${repo}/contents/pf_musico1.png`,
-    `https://api.github.com/repos/${repo}/contents/pf_musico2.png`,
-    `https://api.github.com/repos/${repo}/contents/pf_musico3.png`,
+    `https://api.github.com/repos/${repo}/contents/folder/pf_musico1.png`,
+    `https://api.github.com/repos/${repo}/contents/folder/pf_musico2.png`,
+    `https://api.github.com/repos/${repo}/contents/folder/pf_musico3.png`,
 ];
 
 
@@ -146,4 +148,4 @@ const pit2 = [
 ];
 
 carregarImagens(pit2, 'pit2');
-});
+//});
